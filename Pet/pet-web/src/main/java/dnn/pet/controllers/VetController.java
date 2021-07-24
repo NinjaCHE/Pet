420a3e7dc82e9e92e0d8ceed21d2f078c71219bf
Dan <Dan@DNN-Lenovo>
2021-07-24 22:47:57 +0300

    Vet index and controller

---
+++ Pet/pet-web/src/main/java/dnn/pet/controllers/VetController.java
@@ -0,0 +1,14 @@
+package dnn.pet.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+	@RequestMapping({"/vets", "/vets/", "/vets/index.html", "/vets/index"})
+	public String listVets() {
+		
+		return "vets/index";
+	}
+}
