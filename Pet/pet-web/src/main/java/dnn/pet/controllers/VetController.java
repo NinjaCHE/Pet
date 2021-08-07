955c020a0986edfb9cd725afff1042af26be78e5
Dan <Dan@DNN-Lenovo>
2021-08-08 00:03:37 +0300

    n

--- Pet/pet-web/src/main/java/dnn/pet/controllers/VetController.java
+++ Pet/pet-web/src/main/java/dnn/pet/controllers/VetController.java
@@ -20,7 +20,7 @@ public class VetController {
 
 
 
-	@RequestMapping({"/vets", "/vets/", "/vets/index.html", "/vets/index"})
+	@RequestMapping({"/vets", "/vets/", "/vets/index.html", "/vets/index", "/vets.html"})
 	public String listVets(Model model) {
 		
 		model.addAttribute("vets", vetService.findAll());
