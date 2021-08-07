da784696f18b96f7779f8ccb752de3dd23c25bb5
Dan <Dan@DNN-Lenovo>
2021-08-08 00:28:21 +0300

    gf

--- Pet/pet-web/src/main/java/dnn/pet/bootstrap/DataLoader.java
+++ Pet/pet-web/src/main/java/dnn/pet/bootstrap/DataLoader.java
@@ -4,8 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import dnn.pet.model.Owner;
+import dnn.pet.model.PetType;
 import dnn.pet.model.Vet;
 import dnn.pet.service.OwnerService;
+import dnn.pet.service.PetTypeService;
 import dnn.pet.service.VetService;
 
 @Component
@@ -13,21 +15,23 @@ public class DataLoader implements CommandLineRunner{
 
 	private final OwnerService ownerService;
 	private final VetService vetService;
+	private final PetTypeService petTypeService;
 
-	public DataLoader(OwnerService ownerService, VetService vetService) {
+	public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
 		this.ownerService = ownerService;
 		this.vetService = vetService;
+		this.petTypeService = petTypeService;
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-//		PetType dog = new PetType();
-//        dog.setName("Dog");
-//        PetType savedDogPetType = petTypeService.save(dog);
-//
-//        PetType cat = new PetType();
-//        cat.setName("Cat");
-//        PetType savedCatPetType = petTypeService.save(cat);
+		PetType dog = new PetType();
+		dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
 
 //        Speciality radiology = new Speciality();
 //        radiology.setDescription("Radiology");
